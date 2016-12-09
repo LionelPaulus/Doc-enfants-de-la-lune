c21067aef867f699a2b660f7e6ccdd84e10ef3c7
Adrien Zaganelli <adrienzaganelli@gmail.com>
2016-12-09 11:51:28 +0100

    move fonts

--- gulpfile.js
+++ gulpfile.js
@@ -70,5 +70,5 @@ gulp.task('dev', ['sync', 'sass', 'webpack'], () => {
 gulp.task('default', ["dev"])
 
 gulp.task("importFonts", () => {
-    gulp.src("./app/src/fonts/*").pipe(gulp.dest("./app/dist/fonts/"));
+    gulp.src("./app/src/media/fonts/*").pipe(gulp.dest("./app/dist/media/fonts/"));
 });
