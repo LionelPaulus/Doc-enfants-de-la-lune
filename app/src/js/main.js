fb4bd40a71570d615e5dd226c301e8a66ea1a75c
Adrien Zaganelli <adrienzaganelli@gmail.com>
2016-12-16 03:48:27 +0100

    :bug: :lipstick: fix moon style

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -97,7 +97,7 @@ $('.previous').on('click', () => {
 
 
 const torch = new Torch();
-const moon = new MoonTimeline('-95%');
+const moon = new MoonTimeline('-90%');
 const events = new PageEvents(pages.filter((e) => e.tag === pages[0].tag)[0]);
 //torch.launchCamera();
 
